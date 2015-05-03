0d5b66b1e3d8bad1597f516d51b9ce95353d5530
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2015-05-03 08:33:37 +0200

    Improved Proof-Of-Concept for flush passing to other cluster node

--- app/code/community/JeroenVermeulen/Hosting/Model/Api.php
+++ app/code/community/JeroenVermeulen/Hosting/Model/Api.php
@@ -6,18 +6,21 @@ class JeroenVermeulen_Hosting_Model_Api extends Mage_Api_Model_Resource_Abstract
     public function cacheClean( $mode, $tags=array() ) {
         Mage::log( sprintf('Cache Clean Received via API:  mode:%s  tags:%s',$mode,implode(',',$tags)) );
         $result = false;
+        $message = '';
         Mage::register('JeroenVermeulen_cacheClean_via_Api',true);
         try {
             $result = Mage::app()->getCacheInstance()->getFrontEnd()->getBackend()->clean($mode, $tags);
 //            if ( !$result ) {
-                Mage::log( sprintf("%s::%s: Clean command returned '%s'.", __CLASS__, __FUNCTION__, $result) );
+            $message = sprintf("%s::%s: Clean command returned '%s'.", __CLASS__, __FUNCTION__, $result);
+            Mage::log( $message );
 //                $this->_fault('command_failed', 'Clean command failed.');
 //            }
         } catch (Mage_Core_Exception $e) {
-            Mage::log( sprintf("%s::%s: ERROR %s", __CLASS__, __FUNCTION__, $e->getMessage()) );
-            $this->_fault('command_failed', $e->getMessage());
+            $message = sprintf("%s::%s: ERROR %s", __CLASS__, __FUNCTION__, $e->getMessage());
+            Mage::log( $message );
+            $this->_fault('command_failed', $message);
         }
-        return $result;
+        return array($result,$message);
     }
 
 }
