966ef426609ecc3d41ca7933b1901ae0d854cb98
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2015-05-03 02:39:04 +0200

    Proof-Of-Concept for flush passing to other cluster node

---
+++ app/code/community/JeroenVermeulen/Hosting/Block/Adminhtml/System/Config/Form/Fieldset/First.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * JeroenVermeulen_Hosting
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this Module to
+ * newer versions in the future.
+ *
+ * @category    JeroenVermeulen
+ * @package     JeroenVermeulen_Hosting
+ * @copyright   Copyright (c) 2015 Jeroen Vermeulen (http://www.jeroenvermeulen.eu)
+ */
+/** @noinspection PhpUndefinedClassInspection */
+
+class JeroenVermeulen_Hosting_Block_Adminhtml_System_Config_Form_Fieldset_First
+    extends Mage_Adminhtml_Block_System_Config_Form_Fieldset
+{
+    /**
+     * Show explanation
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
+    protected function _getHeaderHtml($element) {
+        $result = '';
+        $goodBackEnds = array();
+        $currentBackEnd = get_class( Mage::app()->getCacheInstance()->getFrontEnd()->getBackend() );
+        $currentBackEnd = preg_replace( '/^Zend_Cache_Backend_/','', $currentBackEnd );
+        $message = '';
+        $dependClasses = array('Cm_Cache_Backend_File', 'Cm_Cache_Backend_Redis');
+        $optionClasses = array();
+        $or = "' " . $this->__('or') . " '";
+        foreach( $dependClasses as $dependClass ) {
+            $ourClass = 'JeroenVermeulen_' . $dependClass;
+            if ( mageFindClassFile($dependClass) ) {
+                $goodBackEnds[] = $ourClass;
+            } else {
+                $optionClasses[$dependClass] = $ourClass;
+            }
+        }
+        if ( empty($goodBackEnds) ) {
+            $message .= 'ERROR:';
+            $message .= '<br />' . $this->__("This extension requires one of these classes to exist: '%s'", join($or,$dependClasses));
+        } elseif ( ! in_array( $currentBackEnd, $goodBackEnds ) ) {
+            $message .= 'ERROR:';
+            $message .= '<br />' . $this->__("This extension requires cache backend: '%s'", join($or,$goodBackEnds) );
+            $message .= '<br />' . $this->__("Current setting: '%s'", $currentBackEnd);
+            $message .= '<br />';
+            foreach( $optionClasses as $dependClass => $ourClass ) {
+                $message .= '<br />' . $this->__("If you would install '%s' you could also use '%s'.", $dependClass, $ourClass );
+            }
+        }
+        if ( !empty($message) ) {
+            $result.= sprintf( '<ul class="messages"><li class="error-msg"><ul><li><span>%s</span></li></ul></li></ul>', $message );
+        }
+        $result .= parent::_getHeaderHtml( $element );
+        return $result;
+    }
+
+}
--- app/code/community/JeroenVermeulen/Hosting/Model/Observer.php
+++ app/code/community/JeroenVermeulen/Hosting/Model/Observer.php
@@ -2,17 +2,52 @@
 
 class JeroenVermeulen_Hosting_Model_Observer
 {
+    const CONFIG_SECTION  = 'jeroenvermeulen_hosting';
 
     /**
      * @param Varien_Event_Observer $observer
      */
     public function cleanMediaCacheAfter( $observer ) {
         $miniDir = Mage::getBaseDir('base') . DIRECTORY_SEPARATOR . 'mini';
-        $success = $this->clean_dir_content( $miniDir );
-        if ( $success ) {
-            Mage::getSingleton('adminhtml/session')->addSuccess( sprintf("Directory '%s' has been cleaned.",$miniDir) );
-        } else {
-            Mage::getSingleton('adminhtml/session')->addError( sprintf("Error cleaning directory '%s'.",$miniDir) );
+        if ( is_dir($miniDir) ) {
+            $success = $this->clean_dir_content( $miniDir );
+            if ( $success ) {
+                Mage::getSingleton('adminhtml/session')->addSuccess( sprintf("Directory '%s' has been cleaned.",$miniDir) );
+            } else {
+                Mage::getSingleton('adminhtml/session')->addError( sprintf("Error cleaning directory '%s'.",$miniDir) );
+            }
+        }
+    }
+
+    /**
+     * @param Varien_Event_Observer $observer
+     */
+    public function jvCleanBackendCache( $observer ) {
+        if ( Mage::getStoreConfigFlag(self::CONFIG_SECTION.'/cluster/enable_pass_cache_clean')
+             && ! Mage::registry('JeroenVermeulen_cacheClean_via_Api') ) {
+            $transport = $observer->getTransport();
+            $nodes = Mage::getStoreConfig(self::CONFIG_SECTION.'/cluster/http_nodes');
+            $url = Mage::getUrl('api/v2_soap',array('_query'=>'wsdl'));
+            $urlData = parse_url($url);
+            foreach ( explode("\n",$nodes) as $node) {
+                $node = trim($node);
+                $nodeSplit = explode(':',$node);
+                $scheme = $urlData['scheme'];
+                if ( !empty($nodeSplit[1]) ) {
+                    if ( 443 == $nodeSplit[1] ) {
+                        $scheme = 'https';
+                    }
+                    elseif ( 80 == $nodeSplit[1] ) {
+                        $scheme = 'http';
+                    }
+                }
+                $locationUrl = $scheme."://".$node.$urlData['path']; // .'?'.$urlData['query'];
+                $client = new Zend_Soap_Client($url);
+                $client->setLocation($locationUrl);
+                //$client->setSoapVersion(SOAP_1_2);
+                $sessionId =  $client->login( 'soapuser', 'soappass' ); // TODO
+                $client->jvHostingCacheClean( $sessionId, $transport->getMode(), $transport->getTags() );
+            }
         }
     }
 
